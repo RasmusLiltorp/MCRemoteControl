2a80ffd9eb35e54ae5dcee014105f005348c6fa5
Rasmus <liltorp03@gmail.com>
2025-03-16 00:18:51 +0100

    initial layout

--- client/App.axaml.cs
+++ client/App.axaml.cs
@@ -1,11 +1,21 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
+using Avalonia.Styling;
 
 namespace client;
 
 public partial class App : Application
 {
+    public static Styles DarkTheme = new Styles
+    {
+        new StyleInclude(new Uri("avares://MCRemoteControl/Styles/Dark.axaml"))
+        {
+            Source = new Uri("avares://Avalonia.Themes.Fluent/FluentDark.axaml")
+        }
+    };
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
